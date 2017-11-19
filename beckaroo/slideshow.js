ff00b52378e61b4d6a5cb9d7e2115de2739aa102
Nick Edwards <nledwa01@gmail.com>
2017-11-19 13:10:31 -0500

    Updated background imageon home page. Added files for beckaroo page.

---
+++ beckaroo/slideshow.js
@@ -0,0 +1,14 @@
+/********************************
+PICTURE CAROUSEL
+*********************************/
+
+$(document).ready(function(){
+  $('.zoo-pictures').slick({
+    autoplay: true,
+    arrows: false,
+    dots: true,
+    fade: true,
+    pauseOnHover: true,
+    pauseOnDotsHover: true
+  });
+});
